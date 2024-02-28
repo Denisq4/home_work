4a6003fbd16eb96a54cc477664539cd4f6350a08
Denis M <denisq4@inbox.ru>
2024-02-28 20:24:44 +0300

    change home-work and main

--- home_work.py
+++ home_work.py
@@ -17,9 +17,7 @@ class Student:
             return 'ошибка'
 
     def get_average_grade_st(self):
-        avg_grade = sum(self.grades.values()) / len(self.grades.values())
-        return avg_grade
-
+        return sum(self.grades.values()) / len(self.grades.values())
     def __str__(self):
         return f'имя: {self.name}\nфамилия: {self.surname}' \
                f'\nсредняя оценка за домашнее задание: {self.get_average_grade_st}\n' \
@@ -40,8 +38,7 @@ class Mentor:
 class Lecturer(Mentor):
     grades = {}
     def get_average_grade_lc(self):
-        avg_grade = sum(self.grades.values()) / len(self.grades.values())
-        return avg_grade
+        return sum(self.grades.values()) / len(self.grades.values())
 
     def __str__(self):
         return f'имя: {self.name}\nфамилия: {self.surname}\nсредняя оценка за лекции: {self.get_average_grade_lc}'
@@ -97,7 +94,7 @@ mentor_2 = Mentor('Михаил', 'Мартюшев')
 mentor_2.courses_attached = 'Java'
 
 reviewer_1.rate_homework(student_2, 'Java', '9')
-reviewer_1.rate_homework(student_1, 'Python', '10')
+reviewer_2.rate_homework(student_1, 'Python', '10')
 student_1.rate_lecture(lecturer_1, 'Python', '8')
 student_2.rate_lecture(lecturer_1, 'Java', '9')
 print('Студенты')
@@ -118,12 +115,8 @@ print(mentor_2)
 
 list_studentov = [student_1, student_2]
 list_lecturer = [lecturer_1, lecturer_2]
-list_course = ['Python', 'Java']
-dict_grades = {'Python': [8, 9, 10, 5],
-               'Java': [6, 4, 3, 10]}
-def comparison(name_course, grades):
-    avg = sum(grades[name_course]) / len(grades[name_course])
-    return avg
+l_course = 'Python'
 
-b = comparison(list_course[1], dict_grades)
-print(b)
+def comparison(list_people, name_course):
+    for i in list_people:
+
